35c6072384a3acb4923d95db8cc1fb6974147b5a
minhongsun <alsghdtjs7@gmail.com>
2022-07-20 17:34:33 +0900

    addtodo 추가

---
+++ react-workspace/todo-react-app/src/AddTodo.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { TextField, Paper, Button, Grid} from "@material-ui/core";
+
+class AddTodo extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {item: { title:""}};
+    }
+
+    onInputChange =(e) => {
+        const thisItem =this.state.item;
+        thisItem.title = e.target.value;
+        this.setState({itme: thisItem});
+        console.log(thisItem);
+    }
+
+    render() {
+        return (
+            <Paper style = {{ margin:16, padding:16}}>
+                <Grid container>
+                    <Grid xs={11} md={11} item style={{ paddingRight: 16 }}>
+                        <TextField 
+                            placeholder = "Add Todo gere" 
+                            fullWidth
+                            onChange={this.onInputChange}
+                            value={this.state.item.title}
+                             />
+                    </Grid>
+                    <Grid xs={1} md={1} item>
+                        <Button fullwidth 
+                            color="secondary" 
+                            variant="outlined">
+                            +
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Paper>
+        )
+    }
+}
+
+export default AddTodo;
