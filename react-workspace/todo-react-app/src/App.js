6c1e28413615d76488c6967f224f94c46964bf10
minhongsun <alsghdtjs7@gmail.com>
2022-07-21 16:17:53 +0900

    config 추가

--- react-workspace/todo-react-app/src/App.js
+++ react-workspace/todo-react-app/src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 import Todo from "./Todo";
 import AddTodo from "./AddTodo.js";
 import { Paper, List, Container } from "@material-ui/core";
+import { call } from "./service/ApiService"
 import "./App.css";
+
 
 class App extends React.Component {
   constructor(props) {
@@ -13,22 +15,66 @@ class App extends React.Component {
   }
 
   add = (item) => {
-    const thisItems = this.state.items;
-    item.id = "ID-" + thisItems.length; // key를 위한 id추가
-    item.done = false; // done 초기화
-    thisItems.push(item); // 배열에 아이템 추가
-    this.setState({ items: thisItems }); // 업데이트는 반드시 this.setState로 해야됨.
-    console.log("items : ", this.state.items);
+
+    call("/todo", "POST", item).then((response) =>
+      this.setState({ items: response.data })
+    );
+
+    // const thisItems = this.state.items;
+    // item.id = "ID-" + thisItems.length; // key를 위한 id추가
+    // item.done = false; // done 초기화
+    // thisItems.push(item); // 배열에 아이템 추가
+    // this.setState({ items: thisItems }); // 업데이트는 반드시 this.setState로 해야됨.
+    // console.log("items : ", this.state.items);
   };
 
   delete = (item) => {
-    const thisItems = this.state.items;
-    console.log("Before Update Items : ", this.state.items);
-    const newItems = thisItems.filter((e) => e.id !== item.id); // 해당 id 걸러내기
-    this.setState({ items: newItems }, () => {
-      // 디버깅 콜백
-      console.log("Update Items : ", this.state.items);
-    });
+
+    call("/todo", "DELETE", item).then((response) =>
+      this.setState({ items: response.data })
+    );
+
+    // const thisItems = this.state.items;
+    // console.log("Before Update Items : ", this.state.items);
+    // const newItems = thisItems.filter((e) => e.id !== item.id); // 해당 id 걸러내기
+    // this.setState({ items: newItems }, () => {
+    //   // 디버깅 콜백
+    //   console.log("Update Items : ", this.state.items);
+    // });
+  };
+  
+  componentDidMount() {
+    
+    call("/todo", "GET", null).then((response) =>
+      this.setState({ items: response.data })
+    );
+
+
+    // const requestOptions = {
+    //   method: "GET",
+    //   headers: {"Content-Type" : "application/json"}
+    // };
+
+    // fetch("http://localhost:8080/todo", requestOptions)
+    //   .then((reponse) => reponse.json())
+    //   .then(
+    //     (reponse) => {
+    //       this.setState({
+    //         items:reponse.data,
+    //       });
+    //     },
+    //     (error) => {
+    //       this.setState({
+    //         error,
+    //       });
+    //     }
+    //   );
+  }
+
+  update = (item) => {
+    call("/todo", "PUT", item).then((response) => 
+      this.setState({ item: response.data})
+    );
   };
 
   render() {
@@ -36,7 +82,11 @@ class App extends React.Component {
       <Paper style={{ margin: 16 }}>
         <List>
           {this.state.items.map((item, idx) => (
-            <Todo item={item} key={item.id} delete={this.delete} />
+            <Todo 
+              item={item} 
+              key={item.id} 
+              delete={this.delete}
+              update={this.update} />
           ))}
         </List>
       </Paper>
