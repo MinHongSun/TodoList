4486eea3aeba6a987c3c159d1c26aba23f985968
minhongsun <alsghdtjs7@gmail.com>
2022-07-28 15:00:40 +0900

    백앤드 배포

--- demo/src/main/java/com/example/demo/controller/HealthCheck.java
+++ demo/src/main/java/com/example/demo/controller/HealthCheck.java
@@ -1,2 +1,13 @@
-package com.example.demo.controller;public class HealthCheck {
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+
+    @GetMapping("/")
+    public String healthCheck() {
+        return "The service is up running...";
+    }
 }
