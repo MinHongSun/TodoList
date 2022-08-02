99114cf81dea946fb88a64855f8341099070b87b
minhongsun <alsghdtjs7@gmail.com>
2022-08-02 11:32:38 +0900

    프론트 엔드 배포

--- react-workspace/todo-react-app/src/app-config.js
+++ react-workspace/todo-react-app/src/app-config.js
@@ -5,5 +5,8 @@ const hostname = window && window.location && window.location.hostname;
 if (hostname === "localhost") {
     backendHost = "http://localhost:8080";
 }
+else {
+    backendHost = "http://todo.us-west-2.elasticbeanstalk.com";
+}
 
 export const API_BASE_URL = `${backendHost}`;
